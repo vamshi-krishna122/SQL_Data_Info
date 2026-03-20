f7483bb7e909c7e09c1e34aff375f7ba25844459
vamshi-krishna122 <137216486+vamshi-krishna122@users.noreply.github.com>
2026-03-20 10:36:18 +0530

    Create 003_insert_sample_data.sql

---
+++ migrations/003_insert_sample_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email) VALUES
+('Amit Sharma', 'amit@example.com'),
+('Priya Singh', 'priya@example.com');
+
+INSERT INTO orders (user_id, amount) VALUES
+(1, 250.50),
+(2, 499.99),
+(1, 120.00);
